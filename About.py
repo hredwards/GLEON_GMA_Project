1aa40876f2ce1e6536936b0429c4b2eed4aaad9e
hredwards <59663636+hredwards@users.noreply.github.com>
2020-03-30 10:27:02 -0500

    added dash dependencies to about.py for callback

--- About.py
+++ About.py
@@ -3,6 +3,7 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from app import app
+from dash.dependencies import Input, Output
 
 
 
