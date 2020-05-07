2c8dc0d82ae4aa8cf2e13ebf6c91c6e2777e27b6
hredwards <59663636+hredwards@users.noreply.github.com>
2020-05-07 13:13:31 -0500

    updated requirements.txt

--- uploadDownload.py
+++ uploadDownload.py
@@ -6,7 +6,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import uuid
 import pandas as pd
 import datetime
 import base64
